f302dfbfb4f0da14032c3440cd96b4697f74efb1
milonte <milonte@gmaim.com>
2019-01-16 11:46:33 +0100

    list of games with search

--- API/IGDB_API.js
+++ API/IGDB_API.js
@@ -1,7 +1,6 @@
 export function getApiDatas(search, fields='*') {
     const API_TOKEN = '2b76c4dac0be6e2aeeda55b1165afff8';
-    /* const URL='https://api-v3.igdb.com/games/?fields='+fields+'&search='+search; */
-    const URL='https://api-v3.igdb.com/games/?filter[name][exixts]=zelda&fields=*';
+    const URL='https://api-v3.igdb.com/games/?fields='+fields+'&search='+search+'&limit=50';
 
     const HEADERS = {
         methods: 'POST',
@@ -12,6 +11,7 @@ export function getApiDatas(search, fields='*') {
     };
     
     return fetch(URL, HEADERS)
+    //.then((resp) => console.log(resp))
     .then((resp) => resp.json())
     .catch((err) =>console.error(err))
 }
--- Components/Search.js
+++ Components/Search.js
@@ -21,50 +21,56 @@ export default class Search extends React.Component {
 
     _orderDatas(games) {
         games.forEach(element => {
-            element.release_dates.sort( (a,b) => {
-                if(a.y<b.y) return-1;
-                if(a.y>b.y) return 1;
-                return 0;
-            });
-        }); 
-        games.sort( (a,b) => {
-            if(a.release_dates[0].y<b.release_dates[0].y) return-1;
-            if(a.release_dates[0].y>b.release_dates[0].y) return 1;
-            return 0;
+            if (element.release_dates) {
+                element.release_dates.sort((a, b) => {
+                    if (a.y < b.y) return -1;
+                    if (a.y > b.y) return 1;
+                    return 0;
+                });
+            }
         });
-
+        games.sort((a, b) => {
+            if (a.release_dates && b.release_dates) {
+                if (a.release_dates[0].y < b.release_dates[0].y) return -1;
+                if (a.release_dates[0].y > b.release_dates[0].y) return 1;
+                return 0;
+            }
+            return 1;
+        });
     }
 
-    _loadDatas() {
-        // use API
-        if (this.state.searchText.length > 0) {
-            let fields = 'name,rating,platforms.name,platforms.generation,popularity,rating,release_dates.m,release_dates.y,cover.url,slug,summary';
-            getApiDatas(this.state.searchText, fields).then(data => {
-                //this._orderDatas(data);
-                this.setState({ games: data });
-            });
-        }
 
-        // or use js Helper
-        /* this._orderDatas(gamesData);
-        this.setState({ games: gamesData }); */
+
+_loadDatas() {
+    // use API
+    if (this.state.searchText.length > 0) {
+        let fields = 'name,rating,platforms.name,platforms.generation,popularity,rating,release_dates.m,release_dates.y,cover.url,slug,summary';
+        getApiDatas(this.state.searchText, fields).then(data => {
+            this._orderDatas(data);
+            this.setState({ games: data });
+        });
     }
-    
-    render() {
-        return (
-            <View style={styles.container}>
-                <TextInput placeholder="Game to search"
-                    style={styles.input}
-                    onChangeText={(text) => this._searchTextInputChanged(text)}></TextInput>
-                <Button title="Search" onPress={(t) => this._loadDatas(t)} />
-                <FlatList
-                    data={this.state.games}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => <GameItem game={item} />}
-                />
-            </View>
-        )
-    }
+
+    // or use js Helper
+    /* this._orderDatas(gamesData);
+    this.setState({ games: gamesData }); */
+}
+
+render() {
+    return (
+        <View style={styles.container}>
+            <TextInput placeholder="Game to search"
+                style={styles.input}
+                onChangeText={(text) => this._searchTextInputChanged(text)}></TextInput>
+            <Button title="Search" onPress={(t) => this._loadDatas(t)} />
+            <FlatList
+                data={this.state.games}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => <GameItem game={item} />}
+            />
+        </View>
+    )
+}
 }
 
 const styles = StyleSheet.create({
