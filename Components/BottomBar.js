e22949c2f149b84f839d472fc2fe32cc19b68c63
milonte <milonte@gmaim.com>
2019-01-16 17:25:58 +0100

    bottomBar

---
+++ Components/BottomBar.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header, Icon, Text } from 'react-native-elements';
+
+export default class BottomBar extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    outerContainerStyles={styles.bottomBarOuter}
+                    innerContainerStyles={styles.bottomBarInner}
+                    leftComponent={
+                        <View>
+                            <Icon name='search' type='font-awesome' color='#5bf' onPress={() => { }} />
+                            <Text style={styles.text}>Search</Text>
+                        </View>
+                    }
+                    centerComponent={
+                        <View>
+                            <Icon name='folder-o' type='font-awesome' color='#5bf' onPress={() => { }} />
+                            <Text style={styles.text}>Library</Text>
+                        </View>
+                    }
+                    rightComponent={
+                        <View>
+                            <Icon name='gear' type='font-awesome' color='#5bf' onPress={() => { }} />
+                            <Text style={styles.text}>Settings</Text>
+                        </View>
+                    }
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    bottomBarOuter: {
+        paddingLeft: 40,
+        paddingRight: 40,
+        height: 60,
+        backgroundColor: 'rgba(0,0,0,.7)',
+    },
+    bottomBarInner: {
+        marginTop: 20,
+    },
+    text: {
+        marginTop: 0,
+        color: 'white',
+    }
+})
